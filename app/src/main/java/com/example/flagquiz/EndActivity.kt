1b04370fdbc1b3c3091eca445414bae1a110d3c9
Darfune <harolep@gmail.com>
2022-03-11 20:30:37 +0200

    Activity for the result

--- app/src/main/java/com/example/flagquiz/EndActivity.kt
+++ app/src/main/java/com/example/flagquiz/EndActivity.kt
@@ -1,21 +1,33 @@
 package com.example.flagquiz
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class EndActivity : AppCompatActivity() {
 
-    private var usernameResultsTextView: TextView? = null
-    private var scoreTextView: TextView? = null
 
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
 
-        usernameResultsTextView = findViewById(R.id.usernameResultsTextView)
-        scoreTextView = findViewById(R.id.scoreTextView)
+        var usernameResultsTextView = findViewById<TextView>(R.id.usernameResultsTextView)
+        var scoreTextView = findViewById<TextView>(R.id.scoreTextView)
+        val retryBtn = findViewById<Button>(R.id.retryBtn)
+        val usernameResults = intent.getStringExtra(Constants.userName)
+        usernameResultsTextView.text = intent.getStringExtra(Constants.userName)
+        scoreTextView.text = intent.getStringExtra(Constants.correctAnswers) + "/10"
 
+        retryBtn.setOnClickListener {
+            val intent = Intent(this, FlagQuizQuestions::class.java)
+            intent.putExtra(Constants.userName,usernameResults)
+            startActivity(intent)
+        }
 
     }
 }
