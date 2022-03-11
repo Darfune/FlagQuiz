f7ee65f0217495803a6749fc39fca0709d337814
Darfune <harolep@gmail.com>
2022-03-11 19:43:20 +0200

    Activity for the result

--- app/src/main/java/com/example/flagquiz/EndActivity.kt
+++ app/src/main/java/com/example/flagquiz/EndActivity.kt
@@ -2,10 +2,20 @@ package com.example.flagquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class EndActivity : AppCompatActivity() {
+
+    private var usernameResultsTextView: TextView? = null
+    private var scoreTextView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
+
+        usernameResultsTextView = findViewById(R.id.usernameResultsTextView)
+        scoreTextView = findViewById(R.id.scoreTextView)
+
+
     }
 }
