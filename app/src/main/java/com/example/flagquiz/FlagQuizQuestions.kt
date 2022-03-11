22a7df0b8e62b2f67531a9f3841faccf183568f4
Darfune <harolep@gmail.com>
2022-03-11 18:42:27 +0200

    Questions Activity

--- app/src/main/java/com/example/flagquiz/FlagQuizQuestions.kt
+++ app/src/main/java/com/example/flagquiz/FlagQuizQuestions.kt
@@ -2,31 +2,241 @@ package com.example.flagquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
-import java.util.*
-import kotlin.random.Random.Default.nextInt
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 
 class FlagQuizQuestions : AppCompatActivity() {
 
-    private var flagImageView: ImageView? = null
     private val flags = Constants.getFlags()
     private val countries = Constants.getCountries()
+    private var flagImageView: ImageView? = null
+    private var progressBar: ProgressBar? = null
+    private var progressTextView: TextView? = null
+    private var optionOneBtn: Button? = null
+    private var optionTwoBtn: Button? = null
+    private var optionThreeBtn: Button? = null
+    private var optionFourBtn: Button? = null
+    private var answer: Int? = null
+    private var progress: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flag_quiz_questions)
 
         flagImageView = findViewById(R.id.flagImageView)
+        progressBar =  findViewById(R.id.progressBar)
+        progressTextView = findViewById(R.id.progressTextView)
+        optionOneBtn = findViewById(R.id.optionOneBtn)
+        optionTwoBtn = findViewById(R.id.optionTwoBtn)
+        optionThreeBtn = findViewById(R.id.optionThreeBtn)
+        optionFourBtn = findViewById(R.id.optionFourBtn)
 
+        createQuestion()
 
+    }
 
+    private fun createQuestion() {
+        val selectedCountry = (0..250).random()
+        var wrongOne : Int
+        var wrongTwo : Int
+        var wrongThree : Int
+        do {
+            wrongOne = (0..250).random()
+        }while (wrongOne == selectedCountry)
+        do {
+            wrongTwo = (0..250).random()
+        }while (wrongTwo == selectedCountry && wrongTwo == wrongOne)
+        do {
+            wrongThree = (0..250).random()
+        }while (wrongThree == selectedCountry && wrongThree == wrongOne && wrongThree == wrongTwo)
 
+        answer = (1..4).random()
+        flagImageView?.setImageResource(flags.getValue(selectedCountry))
+        when (answer){
+           1 -> {
+               optionOneBtn?.text = countries[selectedCountry]
 
-        var id = (0..250).random()
+               optionFourBtn?.text = countries[wrongOne]
+               optionTwoBtn?.text = countries[wrongTwo]
+               optionThreeBtn?.text = countries[wrongThree]
+           }
+           2 -> {
+               optionTwoBtn?.text = countries[selectedCountry]
 
+               optionOneBtn?.text = countries[wrongOne]
+               optionFourBtn?.text = countries[wrongTwo]
+               optionThreeBtn?.text = countries[wrongThree]
+           }
+           3 -> {
+               optionThreeBtn?.text = countries[selectedCountry]
 
-        flagImageView?.setImageResource(flags.getValue(id))
+               optionOneBtn?.text = countries[wrongOne]
+               optionTwoBtn?.text = countries[wrongTwo]
+               optionFourBtn?.text = countries[wrongThree]
+           }
+           4 -> {
+               optionFourBtn?.text = countries[selectedCountry]
+
+               optionOneBtn?.text = countries[wrongOne]
+               optionTwoBtn?.text = countries[wrongTwo]
+               optionThreeBtn?.text = countries[wrongThree]
+           }
+
+        }
+
+    }
+
+    fun selectedAnswer(view: View) {
+        when (view.id){
+            R.id.optionOneBtn ->{
+                if (answer == 1){
+                    optionOneBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.correct_selected_option_bg
+                    )
+                }
+                else{
+                    optionOneBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.wrong_selected_option_bg
+                    )
+                    when (answer){
+                        2 -> optionTwoBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        3 -> optionThreeBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        4 -> optionFourBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                    }
+                }
+            }
+            R.id.optionTwoBtn ->{
+                if (answer == 2){
+                    optionTwoBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.correct_selected_option_bg
+                    )
+                }
+                else{
+                    optionTwoBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.wrong_selected_option_bg
+                    )
+                    when (answer){
+                        1 -> optionOneBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        3 -> optionThreeBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        4 -> optionFourBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                    }
+                }
+            }
+            R.id.optionThreeBtn ->{
+                if (answer == 3){
+                    optionThreeBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.correct_selected_option_bg
+                    )
+                }
+                else{
+                    optionThreeBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.wrong_selected_option_bg
+                    )
+                    when (answer){
+                        2 -> optionTwoBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        1 -> optionOneBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        4 -> optionFourBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                    }
+                }
+            }
+            R.id.optionFourBtn ->{
+                if (answer == 4){
+                    optionFourBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.correct_selected_option_bg
+                    )
+                }
+                else{
+                    optionFourBtn?.background =  ContextCompat.getDrawable(
+                        this,
+                        R.drawable.wrong_selected_option_bg
+                    )
+                    when (answer){
+                        2 -> optionTwoBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        3 -> optionThreeBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                        1 -> optionOneBtn?.background =  ContextCompat.getDrawable(
+                            this,
+                            R.drawable.corrent_not_selected_bg
+                        )
+                    }
+                }
+            }
+        }
+
+        optionOneBtn?.isClickable = false
+        optionTwoBtn?.isClickable = false
+        optionThreeBtn?.isClickable = false
+        optionFourBtn?.isClickable = false
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            optionOneBtn?.background =  ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+            optionTwoBtn?.background =  ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+            optionThreeBtn?.background =  ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+            optionFourBtn?.background =  ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+            createQuestion()
+            optionOneBtn?.isClickable = true
+            optionTwoBtn?.isClickable = true
+            optionThreeBtn?.isClickable = true
+            optionFourBtn?.isClickable = true
+        }, 1500)
 
 
     }
