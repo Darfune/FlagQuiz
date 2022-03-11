5597b9f0de74595edaf7be190a55cbdf9c334797
Darfune <harolep@gmail.com>
2022-03-11 14:16:39 +0200

    updated

--- app/src/main/java/com/example/flagquiz/FlagQuizQuestions.kt
+++ app/src/main/java/com/example/flagquiz/FlagQuizQuestions.kt
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import java.util.*
+import kotlin.random.Random.Default.nextInt
 
-val flags = hashMapOf<Int, String?>()
 
 class FlagQuizQuestions : AppCompatActivity() {
+
+    private var flagImageView: ImageView? = null
+    private val flags = Constants.getFlags()
+    private val countries = Constants.getCountries()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flag_quiz_questions)
 
+        flagImageView = findViewById(R.id.flagImageView)
+
+
+
+
+
+        var id = (0..250).random()
+
+
+        flagImageView?.setImageResource(flags.getValue(id))
+
 
     }
 
-//    private fun questionsGeneration() {
-//        TODO("Not yet implemented")
-//
-//    }
 }
